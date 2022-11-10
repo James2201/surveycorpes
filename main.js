05fca5758b9d9e14a8da4e0d351ec5e2ecfcc701
James2201 <112659518+James2201@users.noreply.github.com>
2022-11-10 14:24:03 -0700

    Update main.js

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ for (let i = 0; i < result.length; i++){
     } else {
         may = may + 1;
     }
-}
+} 
 let adult = 0;
 let child = 0;
-
+ 
 let age = ["1", "18", "35"];
 for (let i = 0; i < age.length; i++){
     if (age[i] >= 18){
@@ -21,12 +21,12 @@ for (let i = 0; i < age.length; i++){
     } else {
         child = child + 1;
     }
-}
+} 
 let cost0 = 0;
 let cost20 = 0;
 let cost50 = 0;
-
-
+ 
+ 
 let prices = ["1", "20", "30", "40", "50"];
 for (let i = 0; i < prices.length; i++){
     if (prices[i] < 20){
@@ -36,22 +36,22 @@ for (let i = 0; i < prices.length; i++){
     } else {
         cost50 = cost50 + 1;
     }
-}
-
+} 
+ 
 function add2(){
     for (let i = 0; i < prices.length; i++){
         prices[i] = prices[i] + 2;
     }
-}
-
+} 
+ 
 function plus5precent(){
     for (let i = 0; i < prices.length; i++){
         prices[i] = prices[i] * 1.05;
     }
-}
-
+} 
+ 
 function minus20percent(){
     for (let i = 0; i < prices.length; i++){
         prices[i] = prices[i] * 0.8;
     }
-}
+} 
